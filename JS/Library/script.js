fcefd7192efd17e0e2a9ed8b2401d71a8e45e0fe
ismaC95 <ismaelcarayol@gmail.com>
2025-06-16 19:10:01 +0100

    Fully functional

--- JS/Library/script.js
+++ JS/Library/script.js
@@ -3,12 +3,20 @@ const app = document.getElementById("app-container");
 const displayBook = document.getElementById("book-display");
 const addBookBtn = document.getElementById("add-book");
 const addBookModal = document.getElementById("add-book-modal");
+const cancelAddBook = document.getElementById("cancel-book");
+const addBookForm = document.getElementById("add-book-form");
 
-function Book(id, title, author) {
+function Book(id, title, author, read = false) {
   this.id = id;
   this.title = title;
   this.author = author;
+  this.read = read;
 }
+
+Book.prototype.readToggle = function () {
+  this.read = !this.read;
+  console.log(myLibrary);
+};
 
 function addBookToLibrary(inputTitle, inputAuthor) {
   const inputId = crypto.randomUUID();
@@ -26,12 +34,40 @@ function displayBooks() {
     const bookCardDisplay = document.createElement("div");
     const titleDisplay = document.createTextNode(`${book.title}`);
     const authorDisplay = document.createTextNode(`${book.author}`);
+    const removeButton = document.createElement("button");
+    const readButton = document.createElement("button");
+
+    removeButton.textContent = "Remove";
+    removeButton.setAttribute("data-id", book.id);
+    removeButton.setAttribute("id", "remove-btn");
+
+    readButton.setAttribute("id", "read-button");
+    readButton.textContent = book.read ? "Mark as Unread" : "Mark as Read";
 
     bookCardDisplay.appendChild(titleDisplay);
     bookCardDisplay.appendChild(document.createElement("br"));
     bookCardDisplay.appendChild(authorDisplay);
+    bookCardDisplay.appendChild(readButton);
+    bookCardDisplay.appendChild(removeButton);
 
     displayBook.appendChild(bookCardDisplay);
+
+    // REMOVE BUTTON FUNCTIONALITY
+    removeButton.addEventListener("click", (e) => {
+      const bookId = e.target.getAttribute("data-id");
+
+      const index = myLibrary.findIndex((book) => book.id === bookId);
+      if (index !== -1) {
+        myLibrary.splice(index, 1);
+      }
+      displayBooks();
+    });
+
+    // READ BUTTON FUNCTIONALITY
+    readButton.addEventListener("click", () => {
+      book.readToggle();
+      displayBooks();
+    });
   });
 }
 
@@ -39,7 +75,24 @@ addBookToLibrary("Hello Kitty", "Miau");
 addBookToLibrary("Mi caballero", "Tania");
 
 addBookBtn.onclick = () => {
-  //   addBookModal.m;
+  addBookModal.showModal();
 };
 
+cancelAddBook.onclick = () => {
+  addBookModal.close();
+};
+
+//add books through the modal
+addBookForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const title = addBookForm.elements["title"].value;
+  const author = addBookForm.elements["author"].value;
+
+  addBookToLibrary(title, author);
+  addBookModal.close();
+  console.log(myLibrary);
+  displayBooks();
+});
+
 displayBooks();
+console.log(myLibrary);
